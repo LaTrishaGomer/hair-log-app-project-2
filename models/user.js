1d2e3914301ad4d44628d55fba69add28367584b
latrisha <l_gomer@yahoo.com>
2025-03-17 16:33:59 -0700

    wip: define hairlog schema

--- models/user.js
+++ models/user.js
@@ -1,6 +1,68 @@
 const mongoose = require('mongoose');
 
-const userSchema = mongoose.Schema({
+
+const hairlogSchema = new mongoose.Schema({
+  date: {
+    type: Date,
+    required: true,
+  },
+  hairCondition: {
+    type: String, 
+    enum: [
+      'dry', 
+      'moisturized', 
+      'oily', 
+      'balanced'],
+  },
+  scalpCondition: {
+    type: String, 
+    enum: [
+      'dry', 
+      'normal', 
+      'oily', 
+      'flaky'],
+  },
+  washDay: {
+    type: Boolean, 
+    default: false,
+  },
+  protectiveStyle: {
+    type: String,
+  },
+  productsUsed: {
+    type: [String],
+  },
+  breakageOrShedding: {
+    type: String, 
+    enum: [
+      'none', 
+      'minimal', 
+      'moderate', 
+      'excessive'],
+  },
+  proteinMoistureBalance: {
+    type: String, 
+    enum: [
+      'protein overload', 
+      'moisture overload', 
+      'balanced'],
+  },
+  weather: {
+    type: String,
+    enum: [
+      'humid', 
+      'dry',
+      'hot', 
+      'cold'], 
+  },
+  notes: {
+    type: String, 
+  },
+});
+
+
+
+const userSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
@@ -9,6 +71,7 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  hairlogs: [hairlogSchema], 
 });
 
 const User = mongoose.model('User', userSchema);
